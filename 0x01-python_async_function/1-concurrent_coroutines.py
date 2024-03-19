cf73b4aa22a1b0a6abd3fece82c0e8876e711f74
nokekip <kipnoke@gmail.com>
2024-03-20 01:27:09 +0300

    removed whitespace

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order without
 using sort() because of concurrency.
 """
- 
+
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
